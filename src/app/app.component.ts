b83f45e744b6f8a52c3d724c152eb7c50e1c2fbb
Keith-Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-01-26 12:33:43 -0500

    Added Auth0 Login
    starting Auth0 Login button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nwktheory';
+  public isAuthenticated = false;
+
+  public logout(): void{
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Angular Material Imports
-import { FormsModule } from '@angular/forms';
+// Auth0 Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/auth-button.component'; 
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
+    // Import the Auth 0
+    AuthModule.forRoot({
+      domain: 'dev-uw446xx8ru35160g.us.auth0.com',
+      clientId: 'EPgxMGFPrviheSCBzWfS73nHejI2paI7',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
