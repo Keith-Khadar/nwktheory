ca058f6fba6e399db9c2d9d195eb7486dc3722f3
alexchristy <a.christy@ufl.edu>
2023-02-09 11:51:58 -0500

    Added map to user struct to hold connections.

--- src/server/types/user.go
+++ src/server/types/user.go
@@ -4,6 +4,7 @@ type User struct {
 	UserID string `json:"UserId"`
 	Name   string `json:"Name"`
 	Email  string `json:"Email"`
+	Connections map[string]float32 `json:"Connections"`
 }
 
 func ValidateUser(u *User) bool { return true }
