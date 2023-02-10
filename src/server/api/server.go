b743ed03486f2a470034c5f07bbe2da60d4c7244
Nareshp1 <nareshp0331@yahoo.com>
2023-02-10 15:06:20 -0500

    Refactored functions to identify user by email instead of ID

--- src/server/api/server.go
+++ src/server/api/server.go
@@ -24,7 +24,7 @@ func (s *Server) Start() error {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", homePage)
-	router.HandleFunc("/users/{id}", s.handleGetUserByID).Methods("GET")
+	router.HandleFunc("/users/{email}", s.handleGetUserByEmail).Methods("GET")
 	router.HandleFunc("/users", s.handleCreateUser).Methods("POST")
 	router.HandleFunc("/users/{email}/connections", s.handleCreateUserConnection).Methods("POST")
 	return http.ListenAndServe(s.listenAddr, router)
