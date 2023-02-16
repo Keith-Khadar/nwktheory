c73925aeed5dc708d0cdf392ebddb78224754a3e
alexchristy <a.christy@ufl.edu>
2023-02-16 12:34:31 -0500

    New function in storage interface UpdateUser

--- src/server/storage/storage.go
+++ src/server/storage/storage.go
@@ -5,6 +5,7 @@ import "server/types"
 type Storage interface {
 	Get(string) (*types.User, error)
 	InsertUser(types.User) error
+	UpdateUser(types.User) error
 	InsertConnection(string, types.Connection) error
 	DeleteUser(Email string) error
 	DeleteConnection(UserEmail string, SourceUser string, DestinationUser string) error
