93150d30911d52a664f20cfc6c9966b2f845e6e3
Eric Zhou <ericzhou@Erics-MacBook-Air.local>
2023-02-16 12:54:04 -0500

    Adding more data points

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -14,33 +14,78 @@ export class GraphComponent implements AfterViewInit {
     this.createChartNWK();
   }
 
-  private getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-
   private createChartNWK(): void {
     const chart = Highcharts.chart('chart-nwk', {
+      title: {
+        text: 'NWK Theory'
+      },
       chart: {
-          type: 'networkgraph'
+          type: 'networkgraph',
+          height: '500'
+      },
+      credits: {
+        enabled: false
       },
       plotOptions: {
-          networkgraph: {
-              layoutAlgorithm: {
-                  linkLength: 50 // in pixels
-              },
-              link: {
-                  color: 'red'
-              }
+        networkgraph: {
+          keys: ['from', 'to'],
+          layoutAlgorithm: {
+            enableSimulation: true,
+            friction: -0.9
           }
+        }
       },
-      series: [{
+      series: [
+        {
+          marker: {
+            radius: 10
+          },
+  
+          type: 'networkgraph',
+          dataLabels: {
+            enabled: true
+          },
           data: [
-              ['A', 'B'],
-              ['B', 'C'],
-              ['C', 'A']
+            ['Miracle Mile Medical Center', 'Samer Alaiti M.D.'],
+            ['Miracle Mile Medical Center', 'Platinum Toxicology'],
+            ['Vantage Toxicology Management', 'Platinum Toxicology'],
+            ['West Oaks Orthopaedic', 'Southern California Orthopedic Institute'],
+            ['Chaparral Medical Group Inc', 'Internal Medicine Medical Group'],
+            ['Vantage Toxicology Management', 'Internal Medicine Medical Group'],
+            ['Chaparral Medical Group Inc', 'West Oaks Orthopaedic'],
+            ['Miracle Mile Medical Center', 'Platinum Toxicology'],
+            ['Vantage Toxicology Management', 'Andrew D Rah MD'],
+            ['Vantage Toxicology Management', 'Norman N Nakata MD'],
+            ['Vantage Toxicology Management', 'Joanne Halbrecht M.d'],
+            ['Vantage Toxicology Management', 'Ronald J Gowey MD'],
+            ['Vantage Toxicology Management', 'Mohammad Sirajullah MD'],
+            ['West Oaks Orthopaedic', 'Chaparral Medical Group Inc'],
+            [
+              'West Oaks Orthopaedic',
+              'Hospitalists Corporation of Inland Empire'
+            ],
+            ['West Oaks Orthopaedic', 'Robert L Horner M.D.'],
+            ['West Oaks Orthopaedic', 'Mark H Hyman MD Inc'],
+            ['Chaparral Medical Group Inc', 'Precision Occ MED Grp Inc.'],
+            ['Precision Occ MED Grp Inc.', 'Gary Phillip Jacobs MD Inc'],
+            ['Precision Occ MED Grp Inc.', 'Wellness Wave LLC'],
+            ['Precision Occ MED Grp Inc.', 'Precision Occ MED Grp Inc'],
+            [
+              'Precision Occ MED Grp Inc.',
+              'Precision Occupational Medical Group, Inc.'
+            ],
+            ['Precision Occ MED Grp Inc.', 'Samer Alaiti MD Inc'],
+            ['Precision Occ MED Grp Inc.', 'Lotus Laboratories'],
+            ['Precision Occ MED Grp Inc.', 'Ontario Medical Center L'],
+            ['Precision Occ MED Grp Inc.', 'Leo Newman'],
+            ['Precision Occ MED Grp Inc.', 'Ca Diagnostic Specialists Inc'],
+            ['Precision Occ MED Grp Inc.', 'Physiolink'],
+            ['Precision Occ MED Grp Inc.', 'Matrix Rehabilitation Inc'],
+            ['Precision Occ MED Grp Inc.', 'Kaiser Foundation Hospitals']
           ]
-      }]
-    } as any);
+        }
+      ]
+    } as Highcharts.Options);
 
     /*
     setInterval(() => {
