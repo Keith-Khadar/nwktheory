a8e00c16eb999de860dca316247668044d34a29d
Keith-Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-02-23 10:06:06 -0500

    Added HTTP Requests
    Added the ability for the front end to make http requests

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,90 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { UsersService } from './users.service';
+export interface User{
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+  providers: [UsersService]
+})
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+  editUser: User | undefined; // the user currently being edited
+  userName = '';
+
+  constructor(private UsersService: UsersService) {}
+
+  @ViewChild('userEditInput')
+  set userEditInput(element: 
+    ElementRef<HTMLInputElement>) {
+      if (element){
+        element.nativeElement.focus();
+      }
+    }
+  
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.UsersService.getUsers()
+      .subscribe(users => (this.users = users));
+  }
+
+  add(name: string): void {
+    this.editUser = undefined;
+    name = name.trim();
+    if(!name){
+      return;
+    }
+
+    // The server will generate the id for this new user
+    const newUser: User = { name } as User;
+    this.UsersService
+      .addUser(newUser)
+      .subscribe(user => this.users.push(user));
+  }
+
+  delete(user: User): void {
+    this.users = this.users.filter(u => u !== user);
+    this.UsersService
+      .deleteUser(user.id)
+      .subscribe();
+  }
+
+  edit(userName: string){
+    this.update(userName);
+    this.editUser = undefined;
+  }
+
+  search(searchTerm: string) {
+    this.editUser = undefined;
+    if(searchTerm){
+      this.UsersService
+        .searchUsers(searchTerm)
+        .subscribe(users => (this.users = users));
+    } else{
+      this.getUsers();
+    }
+  }
+
+  update(userName: string){
+    if(userName && this.editUser && this.editUser.name !== userName){
+      this.UsersService
+        .updateUser({...this.editUser, name: userName})
+        .subscribe(user => {
+          // replace the user in the heroes list with update from server
+          const ix = user ? this.users.findIndex(u => u.id === user.id): -1;
+          if(ix > -1){
+            this.users[ix] = user;
+          }
+        });
+        this.editUser = undefined;
+    }
+  }
+}
---
+++ src/app/users/users.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs';
+
+import { HttpErrorHandler, HandleError } from './../http-error-handler.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
+
+export interface User{
+  id: number;
+  name: string;
+}
+
+@Injectable()
+export class UsersService {
+  usersUrl = 'api/user'; // URL to web api
+  private handleError: HandleError;
+
+  constructor(
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorHandler){
+      this.handleError = httpErrorHandler.createHandleError('UserService');
+  }
+
+  // Get users from the server
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl)
+      .pipe(
+        catchError(this.handleError('getUser', []))
+      );
+  }
+
+  // Get users whose name contains search term
+  searchUsers(term: string): Observable<User[]>{
+    term = term.trim();
+
+    // Add safe, URL encoded search perameter if there is a search term
+    const options = term ? 
+    { params: new HttpParams().set('name', term) } :
+      {};
+      return this.http.get<User[]>(this.usersUrl, options)
+        .pipe(
+          catchError(this.handleError<User[]>
+            ('searchUsers', []))
+        );
+    }
+  
+  // Save Methods
+
+  // Post add a new hero to the database
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.usersUrl, user, httpOptions)
+      .pipe(
+        catchError(this.handleError('addUser', user))
+      );
+  }
+
+  // Delete: delete the user from the server
+  deleteUser(id: number): Observable<unknown> {
+    const url = `${this.usersUrl}/${id}`; // Delete api/heroes/id
+    return this.http.delete(url, httpOptions)
+      .pipe(
+        catchError(this.handleError('deleteUser'))
+      );
+  }
+
+  // Put: update the hero on the server. Returns the updated hero upon sucess
+  updateUser(user: User): Observable<User> {
+    httpOptions.headers = 
+      httpOptions.headers.set('Authprization', 'my-new-auth-token');
+    
+    return this.http.put<User>(this.usersUrl, user, httpOptions)
+      .pipe(
+        catchError(this.handleError('updateUser', user))
+      );
+  }
+
+}
