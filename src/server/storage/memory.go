eca049a05be89a0881c0587a88e5748674bbb029
alexchristy <a.christy@ufl.edu>
2023-03-06 15:20:38 -0500

    Fixed connection persistance issue when inserting a connection

--- src/server/storage/memory.go
+++ src/server/storage/memory.go
@@ -7,7 +7,7 @@ import (
 )
 
 type MemoryStorage struct {
-	Users []types.User
+	Users []*types.User
 }
 
 func NewMemoryStorage() *MemoryStorage {
@@ -19,7 +19,7 @@ func (s *MemoryStorage) GetUser(Email string) (*types.User, error) {
 
 	for _, user := range s.Users {
 		if (user.Email == Email) {
-			return &user, err
+			return user, err
 		}
 	}
 
@@ -32,7 +32,7 @@ func (s *MemoryStorage) InsertUser(User *types.User) error {
 
 	if types.ValidateUser(User) {
 		User.Connections = make([]types.Connection, 0)
-		s.Users = append(s.Users, *User)
+		s.Users = append(s.Users, User)
 		fmt.Printf("Inserted user: [Name: %v, Email: %v]\n", User.Name, User.Email)
 	} else {
 		return errors.New("invalid user")
@@ -54,7 +54,7 @@ func (s *MemoryStorage) UpdateUser(Email string, Name string) error {
 
 func (s *MemoryStorage) DeleteUser(Email string) error {
 
-	newUsers := []types.User {}
+	newUsers := []*types.User {}
 
 	// Add every user except for the user to be deleted
 	for _, user := range s.Users {
@@ -68,62 +68,60 @@ func (s *MemoryStorage) DeleteUser(Email string) error {
 	return nil
 }
 
-func (s *MemoryStorage) InsertConnection(Email string, Connection types.Connection) error {
-	// user, err := s.GetUser(Email)
+func (s *MemoryStorage) InsertConnection(Email string, Connection *types.Connection) error {
+	user, err := s.GetUser(Email)
 
-	// // Check if connection format is valid
-	// if !types.ValidateConnection(&Connection) {
-	// 	return errors.New("invalid connection")
-	// }
+	// Check if connection format is valid
+	if !types.ValidateConnection(Connection) {
+		return errors.New("invalid connection")
+	}
 
-	// // Returns error if User with Email does not exist
-	// if err != nil {
-	// 	return err
-	// }
+	// Returns error if User with Email does not exist
+	if err != nil {
+		return err
+	}
 
 
-	// // Check to see if the connection being inserted is a duplicate if not return error
-	// for _, currConnection := range(user.Connections) {
-	// 	if currConnection.SourceUser == Connection.SourceUser &&
-	// 		currConnection.DestinationUser == Connection.DestinationUser {
-	// 			return errors.New("connection already exists")
-	// 		}
-	// }
+	// Check to see if the connection being inserted is a duplicate if not return error
+	for _, currConnection := range(user.Connections) {
+		if currConnection.SourceUser == Connection.SourceUser &&
+			currConnection.DestinationUser == Connection.DestinationUser {
+				return errors.New("connection already exists")
+			}
+	}
 
-	// // Check if destination user exists in the database
-	// _, err = s.GetUser(Connection.DestinationUser)
-	// if err != nil {
-	// 	return errors.New("destination user does not exist")
-	// }
+	// Check if destination user exists in the database
+	_, err = s.GetUser(Connection.DestinationUser)
+	if err != nil {
+		return errors.New("destination user does not exist")
+	}
 
-	// // Add connection to list for the user
-	// user.Connections = append(user.Connections, Connection)
+	// Add connection to list for the user
+	user.Connections = append(user.Connections, *Connection)
 
-	// return err
+	return err
 
-	return nil
 }
 
 func (s *MemoryStorage) DeleteConnection(UserEmail string, SourceUser string, DestinationUser string) error {
-	// user, err := s.GetUser(UserEmail)
+	user, err := s.GetUser(UserEmail)
 
-	// if err != nil {
-	// 	return err
-	// }
+	if err != nil {
+		return err
+	}
 
-	// // Create new array for connections
-	// newConnections := []types.Connection {}
+	// Create new array for connections
+	newConnections := []types.Connection {}
 
-	// // Add all connections except the one that is to be deleted
-	// for _, connection := range user.Connections {
-	// 	if connection.SourceUser == SourceUser && connection.DestinationUser == DestinationUser {
-	// 		continue
-	// 	}
-	// 	newConnections = append(newConnections, connection)
-	// }
+	// Add all connections except the one that is to be deleted
+	for _, connection := range user.Connections {
+		if connection.SourceUser == SourceUser && connection.DestinationUser == DestinationUser {
+			continue
+		}
+		newConnections = append(newConnections, connection)
+	}
 
-	// user.Connections = newConnections
+	user.Connections = newConnections
 
-	// return err
-	return nil
+	return err
 }
