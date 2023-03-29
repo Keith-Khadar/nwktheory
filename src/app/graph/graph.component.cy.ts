607c09533da5a9140947c4258fc82c96ab88f66a
Keith-Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-03-29 19:35:48 -0400

    Fixed the unit tests

--- src/app/graph/graph.component.cy.ts
+++ src/app/graph/graph.component.cy.ts
@@ -1,11 +1,19 @@
 import { GraphComponent } from './graph.component'
 import { IonicModule } from '@ionic/angular'
+import { AuthService, AuthModule } from '@auth0/auth0-angular';
 
 describe('GraphComponent', () => {
   it('mounts', () => {
     cy.mount(`<ion-app><app-graph></app-graph><ion-app>`, {
       declarations: [GraphComponent],
-      imports:[IonicModule.forRoot()]
+      imports:[IonicModule.forRoot(),
+        AuthModule.forRoot({
+          domain: 'dev-uw446xx8ru35160g.us.auth0.com',
+          clientId: 'EPgxMGFPrviheSCBzWfS73nHejI2paI7',
+          authorizationParams: {
+            redirect_uri: window.location.origin
+          }
+        })]
      })
   })
 })
--- src/app/user-profile/user-profile.component.cy.ts
+++ src/app/user-profile/user-profile.component.cy.ts
@@ -1,11 +1,26 @@
+import { MessageService } from './../message.service';
+import { HttpErrorHandler } from './../http-error-handler.service';
+import { PhotoService } from './../services/photo.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserProfileComponent } from './user-profile.component'
 import { IonicModule } from '@ionic/angular'
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
+import { AuthModule } from '@auth0/auth0-angular';
 
 describe('ProfilePageComponent', () => {
   it('mounts', () => {
     cy.mount(`<ion-app><app-user-profile></app-user-profile><ion-app>`, {
       declarations: [UserProfileComponent],
-      imports:[IonicModule.forRoot()]
+      imports:[IonicModule.forRoot(), HttpClientTestingModule,
+        AuthModule.forRoot({
+          domain: 'dev-uw446xx8ru35160g.us.auth0.com',
+          clientId: 'EPgxMGFPrviheSCBzWfS73nHejI2paI7',
+          authorizationParams: {
+            redirect_uri: window.location.origin
+          }
+        })],
+      providers: [HttpClient, PhotoService, HttpErrorHandler, MessageService, AuthService]
      })
   })
 })
