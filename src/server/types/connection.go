222fe7b37d239145ede0f9466620eec4e5681563
Nareshp1 <44296460+Nareshp1@users.noreply.github.com>
2023-03-29 20:42:38 -0400

    fixed connection struct for testing (temp change)

--- src/server/types/connection.go
+++ src/server/types/connection.go
@@ -1,14 +1,14 @@
 package types
 
 type Connection struct {
-	SourceUser string `json:"from" bson:"sourceuser"`
-	DestinationUser string `json:"to" bson:"destinationuser"`
-	Weight float32 `json:"-" bson:"weight"`
+	SourceUser      string  `json:"from" bson:"sourceuser"`
+	DestinationUser string  `json:"to" bson:"destinationuser"`
+	Weight          float32 `json:"weight" bson:"weight"`
 }
 
-func ValidateConnection(c *Connection) bool { 
+func ValidateConnection(c *Connection) bool {
 	if c.SourceUser != "" && c.DestinationUser != "" {
 		return true
 	}
 	return false
- }
+}
