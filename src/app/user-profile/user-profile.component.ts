5a63cdebc2cac12450b73d488d1a6266862ec878
Keith-Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-03-30 12:43:45 -0400

    Added SSL

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -3,6 +3,9 @@ import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 
 import { PhotoService } from '../services/photo.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorHandler } from '../http-error-handler.service';
+import { HandleError } from '../http-error-handler.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,25 +14,37 @@ import { PhotoService } from '../services/photo.service';
 })
 
 
+
+
 export class UserProfileComponent implements OnInit{
+
+  private handleError: HandleError;
+
   constructor(public photoService: PhotoService,
     @Inject(DOCUMENT) public document: Document,
-    public auth: AuthService
-  ) {}
+    public auth: AuthService,
+    private http: HttpClient,
+  httpErrorHandler: HttpErrorHandler
+  ) {this.handleError = httpErrorHandler.createHandleError('ProfileService');}
 
   // Collected from Auth0
   userEmail: string = '';
   userName: string = '';
+
+  imageExt: string = '';
   
   url = 'http://nwk.tehe.xyz:3000/';
+
 
   ngOnInit(): void {
     console.log("t");
     this.auth.user$.subscribe((user) => {
       this.userEmail = user!.email!
       this.userName = user!.name!
-      console.log(this.userEmail);
-      this.photoService.setPhoto(this.url + "static/images/" + this.userEmail + "_profile.png")
+      this.http.get<ProfilePic>(this.url + 'users/' + this.userEmail + '?profilepic=true').subscribe((image) =>{
+        this.imageExt = image.ProfilePic.substring(image.ProfilePic.lastIndexOf('.'));
+        this.photoService.setPhoto(this.url + "static/images/" + this.userEmail + "_profile" + this.imageExt)
+      });
     })
   }
 
@@ -37,3 +52,7 @@ export class UserProfileComponent implements OnInit{
     this.photoService.addNewToGallery(this.url+ "users/" + this.userEmail + "/image");
   }
 }
+
+interface ProfilePic {
+  "ProfilePic" : string
+};
