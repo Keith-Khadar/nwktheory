7e36e0796ae2a9327036f69722055da30f5c252f
Nareshp1 <44296460+Nareshp1@users.noreply.github.com>
2023-04-06 13:36:04 -0400

    Added validation functionality for messages.

--- src/server/types/message.go
+++ src/server/types/message.go
@@ -1,7 +1,14 @@
 package types
 
 type Message struct {
-	User 	string 	`json:"User"`
-	Channel 		string 	`json:"Channel"`
-	Message string 	`json:"Message"`
+	User    string `json:"User"`
+	Channel string `json:"Channel"`
+	Message string `json:"Message"`
+}
+
+func ValidateMessage(m *Message) bool {
+	if m.User != "" && m.Channel != "" && m.Message != "" {
+		return true
+	}
+	return false
 }
