c3f8c734b19d571833cf97a215de3475626f21e7
Nareshp1 <44296460+Nareshp1@users.noreply.github.com>
2023-04-06 11:17:57 -0400

    Updated user struct to store all message channels they are a part of

--- src/server/types/user.go
+++ src/server/types/user.go
@@ -1,14 +1,15 @@
 package types
 
 type User struct {
-	Name   string `json:"Name,omitempty" bson:"name"`
-	Email  string `json:"Email,omitempty" bson:"email"`
-	ProfilePic string `json:"ProfilePic,omitempty"`
+	Name        string       `json:"Name,omitempty" bson:"name"`
+	Email       string       `json:"Email,omitempty" bson:"email"`
+	ProfilePic  string       `json:"ProfilePic,omitempty"`
 	Connections []Connection `json:"Connections,omitempty" bson:"connections"`
+	Channels    []string     `json:"Channels,omitempty" bson:"channels"`
 }
 
-func ValidateUser(u *User) bool { 
-	if  u.Name != "" && u.Email != "" {
+func ValidateUser(u *User) bool {
+	if u.Name != "" && u.Email != "" {
 		return true
 	}
 	return false
