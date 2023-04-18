c95c3ef85c0c7ada79d1894c37a060c10708037f
Eric Zhou <ericzhou@Erics-MacBook-Air.local>
2023-04-18 17:48:44 -0400

    added buttons for pan zooming, smoothened graph visuals, added modals on node clicks, optimized structures

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -6,7 +6,7 @@ import { Subscription } from 'rxjs';
 
 // Custom Services
 import { HttpsService } from '../services/https.service';
-import { Connection } from '../services/info';
+import { Node, Connection} from '../services/info';
 
 
 HighchartsNetworkGraph(Highcharts);
@@ -18,27 +18,38 @@ HighchartsNetworkGraph(Highcharts);
   styleUrls: ['./graph.component.scss'],
 })
 export class GraphComponent implements AfterViewInit {
-
-  ImageURLS: Array<string> = []
-  Connections: Array<Connection> = []
-
-  panZoomConfig: PanZoomConfig = new PanZoomConfig();private panZoomAPI: PanZoomAPI|null = null;
-  private apiSubscription: Subscription = new Subscription();
-  private modelChangedSubscription: Subscription = new Subscription();
-
+  // Https
   constructor(private https: HttpsService) {}
 
+  // Graph States
+  Connections: Array<Connection> = []
+  Nodes: { [key: string]: Node } = {}
+  isModalOpen = false
+
+  // Initialization States
   dataFetched = false
   viewInitialized = false
 
+  // PanZoomAPI
+  panZoomConfig: PanZoomConfig = new PanZoomConfig();
+  private panZoomAPI: PanZoomAPI|null = null;
+  private apiSubscription: Subscription = new Subscription();
+
+  // Fetch Data & Initialize PanZoomConfig
   ngOnInit(): void {
     // Use the custom service to get the user from the database 
     this.https.getUser(true).subscribe(async (user) => {
       // Save the connections gathered from the database into the connections array
       this.Connections = user.Connections;
       const promises = this.Connections.map(async (connection: Connection) => {
+        if (!(connection.from in this.Nodes)) {
+          const url = await this.https.getImageFromUser(connection.from)
+          this.addNode(connection.from, url)
+        }
+        if (!(connection.to in this.Nodes)) {
           const url = await this.https.getImageFromUser(connection.to)
-          this.ImageURLS.push(url)
+          this.addNode(connection.to, url)
+        }
       })
       await Promise.all(promises)
       this.dataFetched = true
@@ -47,12 +58,17 @@ export class GraphComponent implements AfterViewInit {
     this.panZoomConfig.keepInBounds = true;
     this.panZoomConfig.keepInBoundsRestoreForce = 2.0;
     this.panZoomConfig.keepInBoundsDragPullback = 2.0;
-    this.apiSubscription = this.panZoomConfig.api.subscribe( (api: PanZoomAPI) => this.panZoomAPI = api );
-    this.modelChangedSubscription = this.panZoomConfig.modelChanged.subscribe( (model: PanZoomModel) => this.onModelChanged(model));
+    this.apiSubscription = this.panZoomConfig.api.subscribe( (api: PanZoomAPI) => {
+      this.panZoomAPI = api 
+    });
   }
 
-  // panZoom functions
-  onModelChanged(model: PanZoomModel): void {}
+  // Destroy
+  ngOnDestroy(): void {
+    this.apiSubscription.unsubscribe();
+  }
+
+  // PanZoom Functions
   zoomIn(): void {
     if (this.panZoomAPI) {
       this.panZoomAPI.zoomIn();
@@ -63,6 +79,12 @@ export class GraphComponent implements AfterViewInit {
       this.panZoomAPI.zoomOut();
     }
   }
+
+  zoomReset(): void {
+    if (this.panZoomAPI) {
+      this.panZoomAPI.resetView()
+    }
+  }
   
   // Render graph once view initialzed
   ngAfterViewInit(): void {
@@ -70,10 +92,20 @@ export class GraphComponent implements AfterViewInit {
     this.createChartNWK()
   }
 
-  // Destroy subscriptions
-  ngOnDestroy(): void {
-    this.apiSubscription.unsubscribe();
-    this.modelChangedSubscription.unsubscribe();
+  // Add graph node
+  addNode(id: string, imageURL: string): void {
+    this.Nodes[id] = {
+      id: id,
+      marker: {
+        symbol: 'url(' + imageURL + ')',
+        width:'50',
+        height:'50',
+      },
+    }
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen
   }
 
   // Render when data has returned and data initialized
@@ -91,6 +123,9 @@ export class GraphComponent implements AfterViewInit {
       credits: {
         enabled: false
       },
+      accessibility: {
+        enabled: false
+      },
       plotOptions: {
         networkgraph: {
           keys: ['from', 'to'],
@@ -98,6 +133,10 @@ export class GraphComponent implements AfterViewInit {
             enableSimulation: true,
             integration: 'verlet',
             linkLength: 100
+          },
+          allowPointSelection: true,
+          events: {
+            click: () => this.setOpen(true)
           }
         }
       },
@@ -106,25 +145,14 @@ export class GraphComponent implements AfterViewInit {
           radius: 13,
         },
         dataLabels: {
-          enabled: true,
+          enabled: false,
           linkFormat: '',
           allowOverlap: true,style: {
                         textOutline: false 
                     }
         },
         data: this.Connections,
-        nodes: this.Connections.map((connection: Connection, index) => {
-
-          return {
-            id: connection.to,
-            marker: {
-              radius: 100,
-              symbol: 'url(' + this.ImageURLS[index] + ')',
-              width:'50',
-              height:'50'
-            },
-          }
-        })
+        nodes: Object.values(this.Nodes)
       }]
     } as any);
   }
