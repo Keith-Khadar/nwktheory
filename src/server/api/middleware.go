fb87e729f4dd04f0d8537a140d9c79c6c4a5947e
alexchristy <a.christy@ufl.edu>
2023-04-19 17:28:01 -0400

    Added random int func

--- src/server/api/middleware.go
+++ src/server/api/middleware.go
@@ -1,3 +1,20 @@
 package api
 
-func MyMiddleware() {}
+import (
+	"math/rand"
+	"time"
+)
+
+func randomChannelID() string {
+	rand.Seed(time.Now().UnixNano())
+
+	const charset = "0123456789"
+	const length = 10
+	b := make([]byte, length)
+
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(b)
+}
